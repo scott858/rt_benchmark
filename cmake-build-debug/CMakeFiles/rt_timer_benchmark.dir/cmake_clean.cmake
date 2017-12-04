5d646a0d475c9bc5f9fd192b4b598349c121f00f
Scott Lee <scott.lee@nucleusscientific.com>
2017-12-03 21:49:49 -0500

    initial

---
+++ cmake-build-debug/CMakeFiles/rt_timer_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rt_timer_benchmark.dir/main.cpp.o"
+  "rt_timer_benchmark.pdb"
+  "rt_timer_benchmark"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rt_timer_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ toolchains/linaro-eabihf.cmake
@@ -0,0 +1,16 @@
+
+include(CMakeForceCompiler)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION)
+
+#set(CMAKE_C_COMPILER /opt/toolchains/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
+#set(CMAKE_CXX_COMPILER /opt/toolchains/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER /opt/toolchains/arm-eabi-4.6/bin/arm-eabi-gcc)
+set(CMAKE_CXX_COMPILER /opt/toolchains/arm-eabi-4.6/bin/arm-eabi-g++)
+
+#set(CMAKE_FIND_ROOT_PATH )
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
